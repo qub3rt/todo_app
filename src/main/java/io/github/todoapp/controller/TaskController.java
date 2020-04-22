408dfcd55ef21a32e97d527a41b9a6b30a444e1b
qub3rt <hubert.krzeslak@gmail.com>
2020-04-22 13:22:15 +0200

    first config

--- src/main/java/io/github/todoapp/controller/TaskController.java
+++ src/main/java/io/github/todoapp/controller/TaskController.java
@@ -1,9 +1,17 @@
 package io.github.todoapp.controller;
 
+import io.github.todoapp.model.Task;
 import io.github.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @RepositoryRestController
 public class TaskController {
@@ -13,8 +21,18 @@ public class TaskController {
 
     public TaskController(final TaskRepository repository) {
         this.repository = repository;
-
     }
 
-    //tutaj dodaje komentarz do sprawdzenia czy wszystko się pushuje na githuba
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks(){
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+//    @GetMapping(value = "/tasks")
+//    ResponseEntity<List<Task>> readAllTasks(Pageable page){
+//        logger.info("Custom pageable");
+//        return ResponseEntity.ok(repository.findAll(page));
+//    }
+
 }
