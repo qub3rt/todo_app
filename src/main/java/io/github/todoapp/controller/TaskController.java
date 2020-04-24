2b4260f81de5a8c902c267dd7107280bdaaf24c8
qub3rt <hubert.krzeslak@gmail.com>
2020-04-24 20:54:26 +0200

    next commit

--- src/main/java/io/github/todoapp/controller/TaskController.java
+++ src/main/java/io/github/todoapp/controller/TaskController.java
@@ -4,16 +4,17 @@ import io.github.todoapp.model.Task;
 import io.github.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RepositoryRestController
+@RestController
 public class TaskController {
 
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
@@ -29,10 +30,10 @@ public class TaskController {
         return ResponseEntity.ok(repository.findAll());
     }
 
-//    @GetMapping(value = "/tasks")
-//    ResponseEntity<List<Task>> readAllTasks(Pageable page){
-//        logger.info("Custom pageable");
-//        return ResponseEntity.ok(repository.findAll(page));
-//    }
+    @GetMapping(value = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page){
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
 
 }
--- src/main/java/io/github/todoapp/model/TaskRepository.java
+++ src/main/java/io/github/todoapp/model/TaskRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
-    @RestResource(path = "done", rel = "done")
-    List<Task> findByDone(@Param("state") boolean done);
-}
+//    @RestResource(path = "done", rel = "done")
+//    List<Task> findByDone(@Param("state") boolean done);
+    }
